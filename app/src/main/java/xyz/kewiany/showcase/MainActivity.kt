609f41e878702b6bea2e55df348ab2f63385619e
kewiany <kewin.czuprynski@elpassion.pl>
2021-03-26 14:06:49 +0100

    Handle pop back stack

--- app/src/main/java/xyz/kewiany/showcase/MainActivity.kt
+++ app/src/main/java/xyz/kewiany/showcase/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.main_activity.*
+import org.koin.android.ext.android.inject
+import xyz.kewiany.showcase.utils.NavigationCommander
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
+
+    private val navigationCommander by inject<NavigationCommander>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainNavController = navHostFragment.findNavController()
     }
 
-    override fun onBackPressed() = Unit
+    override fun onBackPressed() {
+        navigationCommander.popBackStack()
+    }
 }
 
 var mainNavController: NavController? = null
